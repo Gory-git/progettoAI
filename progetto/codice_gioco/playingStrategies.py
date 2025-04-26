f83cd85c98728f4b6594928670b25fdaea27c2e0
Gory-git <61806971+Gory-git@users.noreply.github.com>
2025-04-26 17:11:47 +0200

    aggiunta codice

---
+++ progetto/codice_gioco/playingStrategies.py
@@ -0,0 +1,280 @@
+import math
+import copy
+import itertools
+import random
+from collections import namedtuple
+
+import numpy as np
+
+from utils4e import vector_add, MCT_Node, ucb
+
+def minimax_search(game, state):
+    """Search game tree to determine best move; return (value, move) pair."""
+
+    player = state.to_move
+
+    def max_value(state):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a))
+            if v2 > v:
+                v, move = v2, a
+        return v, move
+
+    def min_value(state):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a))
+            if v2 < v:
+                v, move = v2, a
+        return v, move
+
+    return max_value(state)
+
+infinity = math.inf
+
+def alphabeta_search(game, state):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = state.to_move
+
+    def max_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    def min_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity)
+
+
+
+def cache1(function):
+    "Like lru_cache(None), but only considers the first argument of function."
+    cache = {}
+    def wrapped(x, *args):
+        if x not in cache:
+            cache[x] = function(x, *args)
+        return cache[x]
+    return wrapped
+
+
+def alphabeta_search_tt(game, state):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = state.to_move
+
+    @cache1
+    def max_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    @cache1
+    def min_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity)
+
+def cutoff_depth(d):
+    """A cutoff function that searches to depth d."""
+    return lambda game, state, depth: depth > d
+
+def zero_alphabeta_search(game, state, cutoff=cutoff_depth(2)):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = state.to_move
+
+    @cache1
+    def max_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return 0, None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta, depth+1)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    @cache1
+    def min_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return 0, None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta, depth + 1)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity, 0)
+
+def h_alphabeta_search(game, state, cutoff=cutoff_depth(2)):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = state.to_move
+
+    @cache1
+    def max_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return h(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta, depth+1)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    @cache1
+    def min_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return h(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta, depth + 1)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity, 0)
+
+
+def h(board, player):
+    """Valuta la posizione del gioco e restituisce un valore euristico per il giocatore."""
+    score = 0
+    avversario = "Red" if player == "Blue" else "Blue"
+
+    # Esempio di valutazione del controllo del territorio
+    player_territory = board.count(player)
+    opponent_territory = sum(1 for row in board.board for cell in row if cell != player)
+
+    score += player_territory - opponent_territory  # Controllo del territorio
+
+    for row in range(5):
+        for col in range(5):
+            # print("\n", board.board[row][col])
+            if board.board[row][col] != None and board.board[row][col][0] == player:
+                score += evaluate_position(row, col, board, player)  # Valuta la posizione dell'unità del giocatore
+            elif board.board[row][col] != None and board.board[row][col][0] != player:
+                score -= evaluate_position(row, col, board, avversario)  # Penalizza la posizione dell'unità dell'avversario
+    '''
+    
+    '''
+
+    return score
+
+
+def evaluate_position(row, col, board, player):
+    """Valuta la posizione di un'unità in base alla sua posizione sulla griglia."""
+    # Implementa la logica per valutare la posizione
+    score = 0
+    if (row, col) in [(0, 0), (0, 4), (4, 0), (4, 4)]:
+        score += 2  # Punteggio alto per gli angoli
+    if row in [0, 4] or col in [0, 4]:
+        score += 1  # Punteggio per le posizioni ai bordi
+    somma_nemici = 0
+    vicini = neighbors(row, col, board)
+
+    # for (r1, c1) in vicini:
+    #     d1 = board.board[r1][c1]
+    #     if d1 != None:
+    #         for (r2, c2) in vicini:
+    #             d2 = board.board[r2][c2]
+    #             if d2 != None:
+    #                 if d1[0] != player and d2[0] != player and d1[1] + d2[1] > somma_nemici and d1[1] + d2[1] < 7:
+    #                     somma_nemici = d1[1] + d2[1]
+    # score += somma_nemici
+    punti = 0
+    for (r, c) in vicini:
+        if board.board[r][c] is not None:
+
+            punti += board.board[r][c][1] if board.board[r][c][1] < 6 else 0
+            if board.board[r][c][1] == 6:
+                punti += 1
+
+            if board.board[r][c][0] == player:
+                score += punti
+            else:
+                score -= punti
+
+    return score
+
+def neighbors(row, col, board):
+    ret = []
+    if col - 1 >= 0:
+        ret.append((row, col - 1))
+    if row + 1 < 5:
+        ret.append((row + 1, col))
+    if row - 1 >= 0:
+        ret.append((row - 1, col))
+    if col + 1 < 5:
+        ret.append((row, col + 1))
+    return ret
+
+
+
